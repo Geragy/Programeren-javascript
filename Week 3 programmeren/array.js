476be820ad84e73d6c8965cb74708d89dd1cf629
Gerald <91065564+Geragy@users.noreply.github.com>
2021-10-08 09:13:46 +0200

    Test

---
+++ Week 3 programmeren/array.js
@@ -0,0 +1,35 @@
+var cats=["Jeremy","Hanna","Freddy",'Pitta'];
+console.log(`The length of the cats array is ${cats.length}`);
+console.log('The 2nd cat is ${cats[1]}');
+for (let i=0;i< cats.length; i++) {
+
+    console.log(`The cat at ${i} is ${cats[i]}`);
+}
+let j =0;
+for (let element of cats){
+    console.log(`The cat at ${j} is ${element}`);
+    j++;
+}
+for(let [j, element] of cats.entries()){
+    console.log(`The cat at ${j}is ${element}`);
+}
+const daysOfWeek=[
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+];
+console.log(`Get Wed: ${daysOfWeek[2]}`);
+function getReadableDayOfTheWeek(){
+    return new Date().getDay();
+}
+console.log(`Get the number of the day: ${getReadableDayOfTheWeek()}`);
+
+let counter = 99;
+do{
+    counter++;
+    console.log(counter);
+}while(counter< 99 );
